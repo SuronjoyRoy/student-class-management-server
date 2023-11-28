99d019e1a533ca2feb1bbe4338e1b9e567908864
SuronjoyRoy <suronjoyroy21@gmail.com>
2023-11-29 04:06:08 +0600

    user and class related api created

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vlvvhig.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,7 +36,66 @@ async function run() {
       const result = await classCollection.insertOne(userData);
       res.send(result)
     })
-    
+
+    app.get('/class/:email', async(req, res) => {
+      const email= req.params.email;
+      const query={email:email};
+      const result = await classCollection.find(query).toArray();
+      res.send(result);
+      // console.log(email,'this is class email');
+      // console.log('this is teacher result',result)
+    })
+
+    app.get('/update-class/:id', async(req, res) => {
+      const id= req.params?.id;
+      const query={_id: new ObjectId(id)};
+      const result = await classCollection.findOne(query);
+      res.send(result);
+      // console.log(email,'this is class email');
+      // console.log('this is teacher result',result)
+    })
+
+    app.get('/class/:id', async(req, res) => {
+      const id= req.params?.id;
+      const query={_id: new ObjectId(id)};
+      const result = await classCollection.updateOne(query).toArray();
+      res.send(result);
+      // console.log(email,'this is class email');
+      // console.log('this is teacher result',result)
+    })
+
+    app.patch('/class/:id', async(req, res) =>{
+      const item = req.body;
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          email: item.email,
+          category: item.category,
+          title: item.title,
+          Details: item.Details,
+          price: item.price,
+          photoURL: item.photoURL,
+         
+        }
+      }
+      const result =  await classCollection.updateOne(filter, updatedDoc);
+      res.send(result)
+    })
+
+    app.delete('/class/:id', async(req, res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const result = await classCollection.deleteOne(query);
+    res.send(result);
+})
+
+app.get('/class', async (req, res) => {
+  const result = await classCollection.find().toArray();
+  res.send(result);
+});
+
     // user related api
     app.post('/users', async(req, res) =>{
       const userData = req.body;
@@ -49,7 +108,36 @@ async function run() {
       const result = await userCollection.findOne(query);
       res.send(result);
     });
+
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
     
+    app.get('/users/admin/:email', async(req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
     // teacher related api
     app.post('/teacher', async(req, res) =>{
       const teacherData = req.body;
